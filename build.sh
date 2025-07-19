19bc30f9e671fd79b5e8602589ff3b828f3b255a
LaGregance <gregoire.taja@gmail.com>
2025-07-19 23:41:32 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 rm -rf .next
 yarn next build
 sed -i '' -E "s/const currentPort = parseInt\\(process\\.env\\.PORT, 10\\) \\|\\| 3000/const currentPort = parseInt(process.env.I18N_EDITOR_PORT, 10) || 25560/" .next/standalone/server.js
+echo "#!/usr/bin/env node\n$(cat .next/standalone/server.js)" > .next/standalone/server.js
+chmod +x .next/standalone/server.js
